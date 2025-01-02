e31b4490c28d49536d393ee028013e75f2c48fbb
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2025-01-02 11:55:31 +0100

    experimented a bit

--- webserver.js
+++ webserver.js
@@ -13,4 +13,12 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-// Run the server
+// Creating another server.
+
+const server2 = http.createServer((request, response) => {
+  response.end(index.html); // This will throw an error because index.html is not defined. SO you cant just create an html file and expect it to be rendered.
+});
+
+server2.listen(5000, hostname, () => {
+  console.log(`Server running at http://${hostname}:5000/`);
+});
