cc1fb862c1c0a3c14447c5ebd6dd2ed3bcef14f9
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2025-01-28 12:20:38 +0100

    imported the external package slugify

--- webserver.js
+++ webserver.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const url = require("url");
 
+const slugify = require("slugify");
+
 const hostname = "127.0.0.1";
 const port = 3000;
 
@@ -13,6 +15,7 @@ const server = http.createServer((request, response) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
 
 // Creating another server.
 
