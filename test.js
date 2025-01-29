fb2baf6b793ef3542347967b8b30c20e9c18b1ca
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2025-01-29 10:24:07 +0100

    made little corrections

--- test.js
+++ test.js
@@ -3,15 +3,18 @@ const url = require("url");
 
 const server = http.createServer((req, res) => {
   const route = req.url;
+
   if (route === "/get") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
     res.end(
-      "Mikel: I like eggs!\n Tope: I am a backend Developer\n Raj: I support manchester United!"
+      "Mikel: I like eggs!\nTope: I am a backend Developer\nRaj: I support Manchester United!"
     );
   } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("Error 404! Page not found");
   }
 });
 
 server.listen(3000, "127.0.0.1", () => {
-  console.log("Server is now liztening for requests at port 3000!");
+  console.log("Server is now listening for requests at port 3000!");
 });
