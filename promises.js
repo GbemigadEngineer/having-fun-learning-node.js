c6af620aa737c03d0d924541a5c0362baf3d54be
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2025-02-18 08:06:50 +0100

    completed a task on promises

--- promises.js
+++ promises.js
@@ -1,5 +1,25 @@
 "use strict";
 
 const superagent = require("superagent");
+const fs = require("fs");
+const { constrainedMemory } = require("process");
 
-
+// fetch the data from the API
+fetch("https://official-joke-api.appspot.com/random_joke")
+  //   if successful in getting the data
+  .then((response) => {
+    response.json().then((result) => {
+      console.log(`${result.setup}, ${result.punchline}`);
+      const jokeObj = {
+        setup: result.setup,
+        punchline: result.punchline,
+        id: result.id,
+      };
+      fs.writeFile("output2.json", JSON.stringify(jokeObj, null, 2), (err) => {
+        console.log(err);
+      });
+    });
+  })
+  .catch((err) => {
+    console.log("error getting data from API");
+  });
